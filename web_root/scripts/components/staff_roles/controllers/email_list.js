b890c9d45a88a6b5980d63976ed6676789bdd9e4
Adrian Carlson <adrian.r.carlson@gmail.com>
2025-03-20 15:53:55 -0500

    start of show columsn

--- web_root/scripts/components/staff_roles/controllers/email_list.js
+++ web_root/scripts/components/staff_roles/controllers/email_list.js
@@ -9,9 +9,18 @@ define(require => {
 		function ($scope, $attrs, $http) {
 			$scope.hello = 'world'
 			$scope.curSchoolId = $attrs.ngCurSchoolId
+			$scope.setShowColumns = () => {
+				$scope.showColumns = {
+					Name: true
+				}
+				if ($scope.curSchoolId == 0) {
+					$scope.showColumns['School'] = true
+				}
+			}
 
 			$scope.loadData = () => {
 				loadingDialog()
+				$scope.setShowColumns()
 				$http({ url: 'json/staffRoles.json', method: 'GET' }).then(res => {
 					$scope.emailListData = res.data
 					closeLoading()
